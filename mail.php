f5f05b050a24b7ae8f979da1776522dc0ab1e7ad
Sheshiyer <sheshnarayan.iyer@gmail.com>
2021-04-10 02:13:00 +0530

    alpha_serverpush

--- mail.php
+++ mail.php
@@ -2,9 +2,9 @@
 
 if ( $_POST ) {
 
-	$frm_name  = 'John Doe'; // your name
-	$recepient = 'john@example.com'; // your e-mail
-	$sitename  = 'Rhye HTML5 Template'; // your site name
+	$frm_name  = 'Senseplay Team'; // your name
+	$recepient = 'mysenseplay@gmail.com'; // your e-mail
+	$sitename  = 'Senseplay_website'; // your site name
 	$subject   = "New contact from \"$sitename\""; // subject template
 
 	$name  = trim( $_POST['visitor_name'] );
