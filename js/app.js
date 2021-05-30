796e8a7271f9baf673f2967c604536f0d8a85982
Sheshiyer <sheshnarayan.iyer@gmail.com>
2021-05-30 15:44:15 +0530

    nft_launch

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
 import { gsap } from "gsap";
 import { PixiPlugin } from "gsap/PixiPlugin.js";
 import { MotionPathPlugin } from "gsap/MotionPathPlugin.js";
+import{ init } from 'emailjs-com';
+init("user_rRFzurKSXDMbqFTFNsMhy");
